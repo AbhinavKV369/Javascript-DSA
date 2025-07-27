22982ac02e5d89297c48c485be89acd538d12d2f
AbhinavKV369 <abhinavkvmayyil@gmail.com>
2025-07-27 22:50:36 +0530

    Learned  recursive binary search and bubble sort and added

---
+++ 3.Sort-Algorithm/1.bubble-sort.js
@@ -0,0 +1,25 @@
+function bubbleSort(arr) {
+  let swapped
+  do{
+    swapped = false;
+for (let i = 0; i < arr.length - 1; i++) {
+    if(arr[i]>arr[i+1]){
+        let temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        swapped = true;
+    }
+  }
+  }while(swapped)
+}
+
+const arr = [1, 2, 3, 9, 7, 6];
+bubbleSort(arr);
+console.log(arr);
+
+
+// let biscuitEaten = 0
+// do{
+//   console.log("I ate buiscuit number",biscuitEaten);
+//   biscuitEaten++
+// }while(biscuitEaten<3)
---
+++ 3.Sort-Algorithm/2.insertion-sort.js
@@ -0,0 +1,6 @@
+function insertionSort(){
+
+}
+
+//Nested loop practice
+
